3b72eed1bbd51a64374c02c35a3769c4498e9a1e
Võ Viết Trường <voviettruong141003@gmail.com>
2024-07-22 10:01:31 +0700

    Django Tutorial Part 7: Sessions framework

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -15,12 +15,14 @@ def index(request):
 
     # The all() is implied by default
     num_authors = Author.objects.count()
+    num_visits = request.session.get("num_visits", 1)
 
     context = {
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
         "num_authors": num_authors,
+        "num_visits": num_visits,
     }
 
     return render(request, "index.html", context=context)
